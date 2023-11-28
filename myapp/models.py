a453031bb66fa6b1c5b5ffc4659971275e46f986
MoniPanecito <monica.chavez.lugo@gmail.com>
2023-11-28 09:48:53 -0700

    version con rest framework

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=200)
+    sensor1 = models.CharField(max_length=10, null=True)
+    sensor2 = models.CharField(max_length=10, null=True)
+    sensor3 = models.CharField(max_length=10, null=True)
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('hello/<str:username>', views.hello),
+    path('',include('myapp.urls')),
 ]
